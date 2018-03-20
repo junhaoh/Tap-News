54b3b19d775286a5b07712c280cd33986f2c9299
Junhao_Huang <jh@junhaohuang.com>
2018-03-20 00:28:28 -0400

    node server login signup passport and middleware with auth checker

--- web-server/server/app.js
+++ web-server/server/app.js
@@ -1,22 +1,43 @@
+var cors = require('cors')
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
-//routes
-var index = require('./routes/index');
-
+var config = require('./config/config.json');
+var passport = require('passport');
 
 var app = express();
+app.use(bodyParser.json());
+
+require('./models/main.js').connect(config.mongoDbUri)
+
+//routes
+var auth = require('./routes/auth')
+var index = require('./routes/index');
+//var news = require('./routes/news')
 
 // view engine setup
 app.set('views', path.join(__dirname, '../client/build/'));
 app.set('view engine', 'jade');
 app.use('/static', express.static(path.join(__dirname, '../client/build/static/')))
 
-app.use(bodyParser.json());
+// Load passport strategies
+app.use(passport.initialize());
+var localSignupStrategy = require('./passport/signup_passport');
+var localLoginStrategy = require('./passport/login_passport');
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
 
+
+//pass authentication checker middleware
+const authCheckMiddleware = require('./middleware/auth_checker')
+//app.use('/news', authCheckMiddleware)
+
+// TODO: remove this after development is done
+app.use(cors())
 
 app.use('/', index);
+app.use('/auth', auth)
+//app.use('/news', news )
 
 
 // catch 404 and forward to error handler
